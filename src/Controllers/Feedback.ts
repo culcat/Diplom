bf09c23ba71aa13a53b04ad3fc1508566a83d9d3
Aleksandr Krainukov <krainukovaleksandr1@gmail.com>
2024-05-16 21:11:42 +0300

    отредактировал запрос на отзывы

--- src/Controllers/Feedback.ts
+++ src/Controllers/Feedback.ts
@@ -3,15 +3,31 @@ import {query} from "express";
 
 export async function showFeedback() {
     const queryText = 'SELECT * FROM feedback';
-    try{
-        const result = await db.manyOrNone(queryText)
-        console.log(result)
-        return result
-    }catch(e){
-    console.error('Error creating user:', e);
-    throw e;
+    try {
+        const feedback = await db.manyOrNone(queryText);
+        const feedbackWithUsernames = await Promise.all(feedback.map(async (item) => {
+            const user = await getUserById(item.user_id);
+            return { ...item, username: user.firstname };
+        }));
+        console.log(feedbackWithUsernames);
+        return feedbackWithUsernames;
+    } catch (e) {
+        console.error('Error getting feedback:', e);
+        throw e;
     }
 }
+
+async function getUserById(userId:number) {
+    const queryText = 'SELECT firstname FROM public."Users" WHERE id = $1';
+    try {
+        const user = await db.one(queryText, [userId]);
+        return user;
+    } catch (e) {
+        console.error('Error getting user by ID:', e);
+        throw e;
+    }
+}
+
 
 
 export async function createFeedback(user_id:number,message:string) {
